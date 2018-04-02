b03f947473fe334fa48c64fbb172f9db2a64b25c
Pauliih <pau.campos89@gmail.com>
2018-04-01 22:01:07 -0300

    conexion API - search movies

--- src/App.js
+++ src/App.js
@@ -7,13 +7,36 @@ import { SearchForm } from './components/SearchForm';
 // 1 Instalamos Bulma: npm install bulma --save --save-exact
 
 class App extends Component {
+  // Inicializamos un state para results como un array vacío
+  state = { results: [] }
+  // creo método _renderResults
+  _handleResults = (results) => {
+    // Actualizo el state del componentes con los nuevos results
+    this.setState({results})
+  }
+
+  // Método _renderResults() para devolver el mapeado de cada pelicula del array
+  _renderResults() {
+    const {results} = this.state;
+    return results.map(movie => {
+     return <p key={movie.imdbID}>{movie.Title}</p> 
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <Title>Search Movies</Title>
         <div className="SearchForm-wrapper">
-          <SearchForm/>
+          {/* Le pasamos la prop onResults para que ejecute los resultados */}
+          <SearchForm onResults={this._handleResults}/>
         </div>
+          {this.state.results.length === 0
+            ? <p>0 results founded...</p>
+            // Creamos un método llamado _renderResults que sólo se ejecuta si tenemos resultados
+            : this._renderResults()
+          }        
       </div>
     );
   }
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+const API_KEY ='189b9b4d';
 export class SearchForm extends Component {
   // Guardaremos lo que escribimos en el input
   state = {
@@ -15,7 +16,21 @@ export class SearchForm extends Component {
   _handleSubmit = (event) => {
     // evitamos el evento de submit
     event.preventDefault()
-    alert(this.state.inputMovie)
+    // extraer inputMovie del state del metodo anterior
+    const { inputMovie } = this.state;
+    // Petición con fetch a API omdb rescatando el valor que escribe el usuario en el input del buscador
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+    // Devuelvo una promesa - transformo la respuesta a json
+    .then(response => response.json())
+    // se devuelve otra promesa
+    .then(results => {
+      // Extraemos la propiedad Search
+      // Destructuring (results.Search y results.totalResults)
+      const { Search, totalResults} = results;
+      console.log({Search, totalResults})
+      // propiedad Search con resultados que tendrá el metodo que actualice el state en App.js
+      this.props.onResults(Search);
+    })
   }
 
   render() {
