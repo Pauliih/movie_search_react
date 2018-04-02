d912e91cbffb382bcf0bce1b54ba1f06cf9a2e17
Pauliih <pau.campos89@gmail.com>
2018-04-02 01:15:48 -0300

    fix 0 results founded in the page

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ import { MoviesList } from './components/MoviesList';
 // 1 Instalamos Bulma: npm install bulma --save --save-exact
 
 class App extends Component {
-  // Inicializamos un state para results como un array vacío
-  state = { results: [] }
+  // Añadimos un state nuevo usedSearch al componente app que evalue si hemos usado la búsqueda
+  state = { usedSearch: false, results: [] }
   // creo método _renderResults
   _handleResults = (results) => {
-    // Actualizo el state del componentes con los nuevos results
-    this.setState({results})
+    // Actualizo el state del componentes con los nuevos results y el state usedSearch a true (ya que usamos el buscador)
+    this.setState({results, usedSearch: true})
+  }
+
+  _renderResults() {    
+    return this.state.results.length === 0
+      ? <p>\u1F914 Sorry! 0 results founded...</p>
+    // Renderizamos pasando la prop movies que tendrá como valor el state results donde estan todas las peliculas encontradas
+    : <MoviesList movies={this.state.results} />
   }
 
   render() {
@@ -25,10 +32,10 @@ class App extends Component {
           {/* Le pasamos la prop onResults para que ejecute los resultados */}
           <SearchForm onResults={this._handleResults}/>
         </div>
-          {this.state.results.length === 0
-            ? <p>0 results founded...</p>
-            // Le pasamos la prop movies que tendrá como valor el state results donde estan todas las peliculas encontradas
-            : <MoviesList movies={this.state.results}/>
+          {/* como al ingresar no hemos buscado una pelicula nos mostrará el mensaje, pero al buscar ejecutará el método _renderResults */}
+          {this.state.usedSearch
+            ? this._renderResults()
+            : <small>Use the form to search a movie</small>
           }        
       </div>
     );
